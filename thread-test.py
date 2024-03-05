2e17dacfd982c0c90ed4344f40bdca22c528c850
Daniel Pereira <72202548+DaniPalma2002@users.noreply.github.com>
2024-03-05 08:33:38 +0000

    Update head-hand-detection.py and thread-test.py

--- thread-test.py
+++ thread-test.py
@@ -1,5 +1,6 @@
 import time
 import cv2
+import threading
 from requests import head
 import yolov5
 from yolo import YOLO
@@ -16,37 +17,48 @@ headModel.line_thickness = 1  # bounding box thickness (pixels)
 
 handModel = YOLO("models/cross-hands.cfg", "models/cross-hands.weights", ["hand"])
 handModel.confidence = 0.2
-handModel.size = 416
+handModel.size = 412
 
-# initialize the video capture object
 cap = cv2.VideoCapture(0)
 
+def detect_heads(frame, results):
+    results[0] = headModel(frame)
+
+def detect_hands(frame, results):
+    _, _, _, results[1] = handModel.inference(frame)
+
 while True:
-    # Record the time before processing the frame
     start_time = time.time()
 
-    ret, frame = cap.read()
-    # frame = cv2.imread('images/3.jpg')
+    #ret, frame = cap.read()
+    frame = cv2.imread('images/3.jpg')
 
-    # head detection ===========================================================
-    results = headModel(frame)
-    table = results.pandas().xyxy[0]
-    # print(f'Number of heads: {len(table)}')
+
+    results = [None, None]
+
+    # Run the head and hand detection in separate threads
+    t1 = threading.Thread(target=detect_heads, args=(frame, results))
+    t2 = threading.Thread(target=detect_hands, args=(frame, results))
+    t1.start()
+    t2.start()
+
+    # # Wait for both threads to finish
+    # while None in results:
+    #     time.sleep(0.01)
+    t1.join()
+    t2.join()
+
+    # Process the results...
+
+    table = results[0].pandas().xyxy[0]
 
     # show detection bounding boxes on image
-    results.render()
+    results[0].render()
 
-    # hand detection ===========================================================
-    width, height, inference_time, results = handModel.inference(frame)
-
-    # # sort by confidence
-    # results.sort(key=lambda x: x[2])
-
-    # how many hands
-    hand_count = len(results)
+    hand_count = len(results[1])
 
     # display hands
-    for detection in results[:hand_count]:
+    for detection in results[1][:hand_count]:
         id, name, confidence, x, y, w, h = detection
         cx = x + (w / 2)
         cy = y + (h / 2)
@@ -62,13 +74,11 @@ while True:
     cv2.putText(frame, f'Heads: {len(table)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     cv2.putText(frame, f'Hands: {hand_count}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-    # Calculate the FPS
-    fps = 1.0 / (time.time() - start_time)
 
-    # Display the FPS on the frame
+
+    fps = 1.0 / (time.time() - start_time)
     cv2.putText(frame, f'FPS: {fps:.2f}', (frame.shape[1] - 200, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    # show the frame to our screen
     cv2.imshow("Frame", frame)
 
     if cv2.waitKey(1) == ord("q"):
